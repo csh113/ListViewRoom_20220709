2da329d65f0a97089c6bc99fac632d4c0b8bf648
shchoi <gllapia@naver.com>
2022-07-09 22:45:54 +0900

    3. 도전과제   1) 가격 : 세자리마다 컴마     - 1억 이상 : 1억 8,500으로 가공     - 1억 미만 : 8,500으로 가공   2) 가운데 문구 : 주소, ?층 형태로 가공     - 양수 (1,2,3,...) => 1층, 2층, 3층     - 0 => 반지하     - 음수 => 지하 1층, 지하 2층

--- app/src/main/java/com/cshka/listviewtest_20220709/adapters/RoomListAdapter.kt
+++ app/src/main/java/com/cshka/listviewtest_20220709/adapters/RoomListAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.cshka.listviewtest_20220709.R
 import com.cshka.listviewtest_20220709.RoomData
+import java.text.DecimalFormat
 
 class RoomListAdapter(
     val mContext : Context,
@@ -23,14 +24,58 @@ class RoomListAdapter(
 
         val row = tempRow!!
 
-        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        var priceTxt = row.findViewById<TextView>(R.id.priceTxt)
         val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
-        val levelTxt = row.findViewById<TextView>(R.id.levelTxt)
+        var levelTxt = row.findViewById<TextView>(R.id.levelTxt)
         val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
 
-        priceTxt.text = mList[position].price.toString()
+//        도전과제
+//        1) 가격 : 세자리마다 컴마
+//
+//        1억 이상 : 1억 8,500으로 가공
+//        1억 미만 : 8,500으로 가공
+//        2) 가운데 문구 : 주소, ?층 형태로 가공
+//
+//        양수 (1,2,3,...) => 1층, 2층, 3층
+//        0 => 반지하
+//        음수 => 지하 1층, 지하 2층
+
+        var dec = DecimalFormat("#,###")
+
+        var priceVal : Int = mList[position].price
+        var pricePrevVal : Int
+        var priceRemVal : Int
+
+//        가격 표시
+        if ( priceVal < 10000 ) {
+            priceTxt.text = dec.format(priceVal)
+        } else {
+//            28000 / 10000 = 2억 + priceTxt.text = dec.format(priceVal)
+            pricePrevVal = priceVal / 10000
+            priceRemVal = priceVal % 10000
+
+            priceTxt.text = pricePrevVal.toString() + " " + dec.format(priceRemVal)
+        }
+
         addressTxt.text = mList[position].address
-        levelTxt.text = mList[position].level.toString()
+
+//        층수 표시
+        var level : Int = mList[position].level
+        var levelVal : String
+
+        if( level > 0 ) {
+            levelVal = level.toString()
+        }
+        else if (level == 0 ) {
+            levelVal = "반지하"
+        }
+        else if ( level < 0 ) {
+            levelVal = "지하" +  level.toString()
+        }
+        else {  levelVal = "알 수 없는"
+        }
+
+        levelTxt.text = levelVal
         descriptionTxt.text = mList[position].description
 
         return row
